697cb9c891ae531f7c964ef1112b159c0168c970
aray <aray.kalimbetova@mail.ru>
2017-06-02 22:13:28 +0600

    leson-6-Операции-2

--- script.js
+++ script.js
@@ -8,6 +8,7 @@
 Объясните разницу в результатах 5-го и 6-го пункта.
 */
 
+//1
 var a, b, c, d, e;
 a = 5;
 b = 7;
@@ -16,3 +17,12 @@ d = 5.5;
 e = 8;
 summ = (a + b)/((c-d)*(a-e*c));
 document.write("<p style='text-align:center; color:red;'>(" + a + "+" + b + ")/((" + c + "-" + d +")(" + a + "-" + e + "*" + c + "))=" + summ + "</p>");
+
+//2
+var str_1 = "17";
+var str_2 = "-5";
+document.write(str_1 + str_2);
+str_1 = Number(str_1);
+str_2 = Number(str_2);
+document.write("<br>")
+document.write(str_1 + str_2);
